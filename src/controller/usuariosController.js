2d686c04102b176f359467cf85ccf48ddcd38cc2
vitoriaAlbuquerque07 <vitoriam.albuquerque00@gmail.com>
2025-06-02 16:53:13 -0300

    updates

--- src/controller/usuariosController.js
+++ src/controller/usuariosController.js
@@ -1,4 +1,7 @@
 const { prisma } = require("../services");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const saltRounds = 10;
 
 async function buscarUsuarios() {
     try {
@@ -30,6 +33,8 @@ async function buscarUmUsuario(id) {
 
 async function criarUsuario(dados) {
     try {
+        let senhaCriptografada = await bcrypt.hash(dados.usuario_senha, saltRounds);
+        dados = {...dados, usuario_senha: senhaCriptografada};
         return await prisma.usuarios.create({
             data: dados
         });
@@ -44,6 +49,8 @@ async function criarUsuario(dados) {
 
 async function editarUsuario(id,dados) {
     try {
+        let senhaCriptografada = await bcrypt.hash(dados.usuario_senha, saltRounds);
+        dados = {...dados, usuario_senha: senhaCriptografada};
         return await prisma.usuarios.update({
             where: {usuario_id: Number(id)},
             data: dados
@@ -58,7 +65,6 @@ async function editarUsuario(id,dados) {
 }
 
 async function apagarUsuario(id) {
-    // return await executarSQL(`DELETE FROM usuarios WHERE usuario_id = ${id}`)
     try {
         return await prisma.usuarios.delete({
             where: {
@@ -71,6 +77,38 @@ async function apagarUsuario(id) {
             mensagem: error.message
         }
     }
+    // return await executarSQL(`DELETE FROM usuarios WHERE usuario_id = ${id}`)
+}
+
+async function login(dados) {
+    try {
+        let usuario = await prisma.usuarios.findFirst({
+            where: {
+                usuario_email: dados.usuario_email
+            }
+        });
+        if(usuario){
+            let senhaComparada = await bcrypt.compare(dados.usuario_senha, usuario.usuario_senha);
+            if(senhaComparada){
+                let token = jwt.sign({ data: usuario.usuario_senha }, process.env.SEGREDO, { expiresIn: '1h' });
+                return {
+                    tipo: "success",
+                    mensagem: "Usuario logado!",
+                    token
+                }
+            }
+        }
+        return {
+            tipo: "warning",
+            mensagem: "usuário ou senha incorretos"
+        }
+    } catch (error) {
+        return {
+            tipo: "error",
+            mensagem: error.message
+        }
+    }
+    // return await executarSQL(`INSERT INTO usuarios (usuario_nome, usuario_preco, usuario_desconto, usuario_imagem, marca_id,categoria_id) VALUES ('${dados.usuario_nome}', ${dados.usuario_preco}, ${dados.usuario_desconto},'${dados.usuario_imagem}', ${dados.marca_id}, ${dados.categoria_id})`)
 }
 
 module.exports = {
@@ -78,5 +116,6 @@ module.exports = {
     buscarUmUsuario,
     criarUsuario,
     apagarUsuario,
-    editarUsuario
+    editarUsuario,
+    login
 }
