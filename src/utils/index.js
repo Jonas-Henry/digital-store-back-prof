eb65d94b841f010fb17e17aa9f7da99b6b17608b
Jonas-Henry <marceloursulino40@gmail.com>
2025-06-03 16:39:29 -0300

    token-authorization

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,12 +1,24 @@
+const jwt = require("jsonwebtoken")
+
 function rotaProtegida(req, res, next){
-    if(!req.headers.authorization){
+    const token = req.headers.authorization;
+    if(!token){
         res.send({
             tipo: "warning",
             mensagem: "Não autorizado"
         });
     }
 
-    next();
+    jwt.verify(token.split(" ")[1], process.env.SEGREDO, (error) => {
+        if(error){
+            res.send({
+                tipo: "warning",
+                mensagem: "token inválido"
+            })
+        }
+        next();
+    })
+
 }
 
 module.exports = {
